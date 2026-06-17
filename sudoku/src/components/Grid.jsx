e1bb5d7e19374def03f6fdd483f6763dafe95f8e
Amreen Shaikh <shaikhamreenabdulsamad@gmail.com>
2026-06-18 01:35:57 +0530

    Implement Sudoku Board UI

--- sudoku/src/components/Grid.jsx
+++ sudoku/src/components/Grid.jsx
@@ -1,5 +1,54 @@
-export const Grid = () =>{
-    return(
-        <div></div>
-    )
-}
+export const Grid = () => {
+  const board = Array(9)
+    .fill(null)
+    .map(() => Array(9).fill(null));
+
+  const puzzle = Array(9)
+    .fill(null)
+    .map(() => Array(9).fill(null));
+
+  /**
+   * [
+   * [null,null,...]
+   * [null,null,...]
+   * [null,null,...]
+   * [null,null,...]
+   * [null,null,...]
+   * [null,null,...]
+   * [null,null,...]
+   * [null,null,...]
+   * [null,null,...]
+   * ]
+   */
+  return (
+    <div className="container">
+      <table className="table">
+        <tbody>
+          {board.map((row, rIdx) => {
+            return(
+            <tr key={rIdx}>
+              {row.map((cell, cIdx) => {
+                const isPrefilled = puzzle[rIdx][cIdx] !== null;
+                return(
+                <td key={cIdx} className="cell">
+                    <input
+                     type="text"
+                     maxLength={1}
+                     value={cell==null ? '' : cell}
+                     readOnly={isPrefilled}
+                     onFocus={()=>{}}
+                     onChange={()=>{}}
+                     onClick={()=>{}}
+
+                      />
+                </td>
+                )
+              })}
+            </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
