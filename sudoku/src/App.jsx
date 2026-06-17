f7cd695f2868cfd73f3c87f8ef8e9e631907f98a
Amreen Shaikh <shaikhamreenabdulsamad@gmail.com>
2026-06-18 02:31:47 +0530

    Added Row Coum and Box Selection Logic

--- sudoku/src/App.jsx
+++ sudoku/src/App.jsx
@@ -1,11 +1,35 @@
 import {Grid}  from './components/Grid';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const board = Array(9)
+    .fill(null)
+    .map(() => Array(9).fill(null));
+
+  const puzzle = Array(9)
+    .fill(null)
+    .map(() => Array(9).fill(null));
+
+
+    //row,col
+const [selected,setSelected] = useState(null);
+
+// const handleInput =(rIdx,cIdx,value)=>{
+//   if(value === "" || (value >= 1 && value <=9 )){
+
+//   }
+// }
+
   return(
     <div style={{textAlign: 'center'}}>
       <h1>Sudoku</h1>
-      <Grid/>
+      <Grid
+        board={board}
+        puzzle={puzzle}
+        selected={selected}
+        setSelected = {setSelected}
+      />
     </div>
   )
 }
--- sudoku/src/components/Grid.jsx
+++ sudoku/src/components/Grid.jsx
@@ -1,12 +1,5 @@
-export const Grid = () => {
-  const board = Array(9)
-    .fill(null)
-    .map(() => Array(9).fill(null));
-
-  const puzzle = Array(9)
-    .fill(null)
-    .map(() => Array(9).fill(null));
-
+import classNames from "classnames";
+export const Grid = ({ board,handleInput, puzzle, selected, setSelected }) => {
   /**
    * [
    * [null,null,...]
@@ -25,26 +18,59 @@ export const Grid = () => {
       <table className="table">
         <tbody>
           {board.map((row, rIdx) => {
-            return(
-            <tr key={rIdx}>
-              {row.map((cell, cIdx) => {
-                const isPrefilled = puzzle[rIdx][cIdx] !== null;
-                return(
-                <td key={cIdx} className="cell">
-                    <input
-                     type="text"
-                     maxLength={1}
-                     value={cell==null ? '' : cell}
-                     readOnly={isPrefilled}
-                     onFocus={()=>{}}
-                     onChange={()=>{}}
-                     onClick={()=>{}}
-
+            return (
+              <tr key={rIdx}>
+                {row.map((cell, cIdx) => {
+                  const isPrefilled = puzzle[rIdx][cIdx] !== null;
+                  //'same-box', 'same-row', 'same-col'
+                  // let classNames = "cell";
+                  // if (selected && rIdx === selected[0]) {
+                  //   classNames += " same-row";
+                  // } else if (selected && cIdx === selected[1]) {
+                  //   classNames += " same-col";
+                  // } else if (
+                  //   selected &&
+                  //   Math.floor(rIdx / 3) === Math.floor(selected[0] / 3) &&
+                  //   Math.floor(cIdx / 3) === Math.floor(selected[1] / 3)
+                  // ) {
+                  //   classNames += " same-box";
+                  // }
+                  return (
+                    <td
+                      key={cIdx}
+                      className={classNames("cell", {
+                        "same-row": selected && rIdx === selected[0],
+                        "same-col": selected && cIdx === selected[1],
+                        "same-box":
+                          selected &&
+                          Math.floor(rIdx / 3) ===
+                            Math.floor(selected[0] / 3) &&
+                          Math.floor(cIdx / 3) === Math.floor(selected[1] / 3),
+                        selected:
+                          selected &&
+                          rIdx === selected[0] &&
+                          cIdx === selected[1],
+                      })}
+                    >
+                      <input
+                        type="text"
+                        maxLength={1}
+                        value={cell == null ? "" : cell}
+                        readOnly={isPrefilled}
+                        onFocus={() => {
+                          setSelected([rIdx, cIdx]);
+                        }}
+                        onChange={(e) => {
+                          handleInput(rIdx,cIdx,e.target.value)
+                        }}
+                        onClick={() => {
+                          setSelected([rIdx, cIdx]);
+                        }}
                       />
-                </td>
-                )
-              })}
-            </tr>
+                    </td>
+                  );
+                })}
+              </tr>
             );
           })}
         </tbody>
