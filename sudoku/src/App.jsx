23866f4985b563e6b7b99f56af38a2b17ad5e40a
Amreen Shaikh <shaikhamreenabdulsamad@gmail.com>
2026-06-18 00:30:49 +0530

    Set up the react js

---
+++ sudoku/src/App.jsx
@@ -0,0 +1,13 @@
+import {Grid}  from './components/Grid';
+import './App.css';
+
+function App() {
+  return(
+    <div style={{textAlign: 'center'}}>
+      <h1>Sudoku</h1>
+      <Grid/>
+    </div>
+  )
+}
+
+export default App
